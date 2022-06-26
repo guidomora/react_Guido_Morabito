c3a3cd2d035d85a3f7267119a4170223cfe0bee9
guidomora <guidomorabito161@hotmail.com>
2022-06-26 19:05:42 -0300

    nav

--- my-app/src/componente/NavBar.js
+++ my-app/src/componente/NavBar.js
@@ -1,29 +1,31 @@
+import React from 'react';
+
 const NavBar = () => {
     return <div>
-        <nav className="nav">
+        <nav>
             <ul className="flex">
                 <li className="lista-nav">
-                    <button className="lista-boton">
+                    <button type="button" className="btn btn-primary lista-boton">
                         <a href="" className="botones-ruta"> Inicio  </a>
                     </button>
                 </li>
                 <li className="lista-nav">
-                     <button className="lista-boton">
+                     <button type="button" className="btn btn-primary lista-boton">
                         <a href="" className="botones-ruta"> Cajas con flores </a>
                     </button>
                 </li>
                 <li className="lista-nav">
-                    <button className="lista-boton">
+                    <button type="button" className="btn btn-primary lista-boton">
                         <a href="" className="botones-ruta"> Condolencias </a>   
                     </button>
                 </li>
                 <li className="lista-nav">
-                    <button className="lista-boton">
+                    <button type="button" className="btn btn-primary lista-boton">
                         <a href="" className="botones-ruta"> Arreglos florales </a>   
                     </button>
                 </li>
                 <li className="lista-nav">
-                    <button className="lista-boton">
+                    <button type="button" className="btn btn-primary lista-boton">
                         <a href="" className="botones-ruta"> Ramos de flores </a>    
                     </button>
                 </li>
