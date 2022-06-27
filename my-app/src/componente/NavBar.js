f9ebb01ba72f280cd0b805eda35d9e957c27d64d
guidomora <guidomorabito161@hotmail.com>
2022-06-27 10:12:50 -0300

    agregue #

--- my-app/src/componente/NavBar.js
+++ my-app/src/componente/NavBar.js
@@ -6,27 +6,27 @@ const NavBar = () => {
             <ul className="flex">
                 <li className="lista-nav">
                     <button type="button" className="btn btn-primary lista-boton">
-                        <a href="" className="botones-ruta"> Inicio  </a>
+                        <a href="#" className="botones-ruta"> Inicio  </a>
                     </button>
                 </li>
                 <li className="lista-nav">
                      <button type="button" className="btn btn-primary lista-boton">
-                        <a href="" className="botones-ruta"> Cajas con flores </a>
+                        <a href="#" className="botones-ruta"> Cajas con flores </a>
                     </button>
                 </li>
                 <li className="lista-nav">
                     <button type="button" className="btn btn-primary lista-boton">
-                        <a href="" className="botones-ruta"> Condolencias </a>   
+                        <a href="#" className="botones-ruta"> Condolencias </a>   
                     </button>
                 </li>
                 <li className="lista-nav">
                     <button type="button" className="btn btn-primary lista-boton">
-                        <a href="" className="botones-ruta"> Arreglos florales </a>   
+                        <a href="#" className="botones-ruta"> Arreglos florales </a>   
                     </button>
                 </li>
                 <li className="lista-nav">
                     <button type="button" className="btn btn-primary lista-boton">
-                        <a href="" className="botones-ruta"> Ramos de flores </a>    
+                        <a href="#" className="botones-ruta"> Ramos de flores </a>    
                     </button>
                 </li>
             </ul>
