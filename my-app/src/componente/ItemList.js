3d862abc43b8615c4e67ce7c38b8acc7beb9714b
guidomora <guidomorabito161@hotmail.com>
2022-07-09 17:29:44 -0300

    corregido

--- my-app/src/componente/ItemList.js
+++ my-app/src/componente/ItemList.js
@@ -1,29 +1,11 @@
-
+import React from "react";
 import Item from "./Item";
 import '../estilos/ItemList.css';
-import { useEffect, useState } from "react";
 
 
 function ItemList ({Items}) {
-    const [carga, setCarga] = useState(true);
-
-    useEffect(()=>{
-        new Promise((resolve, reject) => {
-            if (carga) {
-            setTimeout(setCarga, 2000, false);
-            } else {
-            reject("no carga")
-            }
-        })
-       
-    }, []);    
-
     return (
         <div className='ItemList'>
-            {carga ? (
-                <h2> Cargando productos... </h2>
-            ) : ( <>
-            
                 {Items.map(({id, titulo, descripcion, precio, imagen, stock}) => (
                 <Item
                 id={id}  
@@ -33,12 +15,8 @@ function ItemList ({Items}) {
                 stock={'stock: '+stock} 
                 imagen={imagen}
                 key={id}/>
-                
             ))}
-
-            </>)}
         </div>
-      
     )
 
 };
--- my-app/src/componente/ItemListContainer.js
+++ my-app/src/componente/ItemListContainer.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../estilos/ItemListContainer.css';
-import ItemCount from './ItemCount';
 import ItemList from "./ItemList";
 import productos from '../productos/productos.json'
 
 function ItemListContainer ({greeting}) {
+    const [carga, setCarga] = useState(true);
+
+    useEffect(()=>{
+       const promesa = new Promise((resolve, reject) => {
+            resolve();
+            reject();
+            });
+            promesa
+            .then(function resuelto(){
+                setTimeout(setCarga, 2000, false)
+            })
+            .catch(function rechazado(){
+                console.log("no carga")
+            });
+    }, []);
+    
+
+
     return <div>{greeting}
-        <ItemList Items={productos} />
+        {carga ? (
+            <h2 className='carga'> Cargando productos... </h2>
+            ) : ( <>
+                <ItemList Items={productos} />
+            </>
+        )}
     </div>
 };
 
 export default ItemListContainer;
+
+
+
