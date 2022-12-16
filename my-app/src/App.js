0b7b2e9363ac7239915b2762e69cda6fd60c2784
guidomora <guidomorabito161@hotmail.com>
2022-12-16 17:52:02 -0300

    btn wpp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import NavBar from "./componente/NavBar";
 import Footer from "./componente/Footer";
 import { Outlet } from "react-router-dom";
+import WhatsApp from "./componente/WhatsApp";
 
 function App() {
   return (
     <div>
       <NavBar imagen="LogoReact" />
       <main>
+        <WhatsApp />
         <Outlet />
       </main>
       <footer>
